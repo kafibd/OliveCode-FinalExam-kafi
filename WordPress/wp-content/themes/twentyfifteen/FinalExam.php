8926378a074a61a766cf0ac5ce01a29512261dbd
kafibd <mezan04@gmail.com>
2015-04-29 18:05:11 +0600

    header and footer completed.

--- WordPress/wp-content/themes/twentyfifteen/FinalExam.php
+++ WordPress/wp-content/themes/twentyfifteen/FinalExam.php
@@ -3,6 +3,102 @@
   Template Name:Final Exam
  */
 ?>
+<head>
+	<title>Final exam of OliveCode</title>
+	<link rel="stylesheet" type="text/css" href="../wp-content/themes/twentyfifteen/main-design.css">
+</head>
 <body>
-	<p>Final Exam</p>
+	<div id="outer-cover">
+		<div id="centering-container">
+			<div id="first-one">
+				<img src="../wp-content/themes/twentyfifteen/images/idx-bkphoto1.png" width="988" height="680" />
+			</div>
+			<div id="second-one">
+				<img src="../wp-content/themes/twentyfifteen/images/idx-bkphoto2.png" width="836" height="689" />
+			</div>
+			<div id="third-one">
+				<img src="../wp-content/themes/twentyfifteen/images/idx-bkphoto3.png" width="719" height="542" />
+			</div>			
+		</div>
+		<div id="circle">
+<!-- 			left: 50px; top: 300px; -->
+			<div class="all-circle" style="width: 250px; height: 250px; position: absolute; left: 260px;top: 300px;">
+				<img src="../wp-content/themes/twentyfifteen/photo/bln_photo1.jpg" style = "height: 250px ;margin-left: -62.125748502994px;">
+			</div><!-- 1 -->
+			<div class="all-circle" style="width: 170px; height: 170px; position: absolute;left: 350px; top: 560px;">
+				<img src="../wp-content/themes/twentyfifteen/photo/bln_photo2.jpg" style = "height: 170px; margin-left:-28.3333333333333px;">
+			</div><!-- 2 -->
+			<div class="all-circle" style="width: 220px; height: 220px; position: absolute; left: 390px; top: 790px;">
+				<img src="../wp-content/themes/twentyfifteen/photo/bln_photo3.jpg" style = "height: 220px; margin-left: -44.4943820224719px;">
+			</div><!-- 3 -->
+			<div class="all-circle" style="width: 190px; height: 190px; position: absolute; left: 150px; top: 850px;">
+				<img src="../wp-content/themes/twentyfifteen/photo/bln_photo4.jpg" style="width: 190px; margin-top: -38.57px;">
+			</div><!-- 4 -->
+			<div class="all-circle" style="width: 240px; height: 240px; position: absolute; left: 350px; top: 1050px;">
+				<img src="../wp-content/themes/twentyfifteen/photo/bln_photo5.jpg" style="height: 240px; margin-left: -60.1801801801802px;">
+			</div><!-- 5 -->
+			<div class="all-circle" style="width: 230px; height: 230px; position: absolute; left: 30px; top: 1950px;">
+				<img src="../wp-content/themes/twentyfifteen/photo/bln_photo6.jpg" style="height: 230px; margin-left: -57.6726726726727px;">
+			</div><!-- 6 -->
+			<div class="all-circle" style="width: 200px; height: 200px; position: absolute; left: 300px; top: 2100px;">
+				<img src="../wp-content/themes/twentyfifteen/photo/bln_photo7.jpg" style="width: 200px; margin-top: -40.2px;">
+			</div><!-- 7 -->
+			<div class="all-circle" style="width: 220px; height: 220px; position: absolute; left: 600px; top: 2150px;">
+				<img src="../wp-content/themes/twentyfifteen/photo/bln_photo8.jpg" style="width: 220px; margin-top: -54.56px;">
+			</div><!-- 8 -->
+			<div class="all-circle" style="width: 190px; height: 190px; position: absolute; left: 780px; top: 2700px;">
+				<img src="../wp-content/themes/twentyfifteen/photo/bln_photo9.jpg" style="height: 190px; margin-left: -47.2155688622755px;">
+			</div><!-- 9 -->
+			<div class="all-circle" style="width: 300px; height: 300px; position: absolute; left: 150px; top: 3000px;">
+				<img src="../wp-content/themes/twentyfifteen/photo/bln_photo10.jpg" style="width: 300px; margin-top: -50.1px;">
+			</div><!-- 10 -->
+			<div class="all-circle" style="width: 200px; height: 200px; position: absolute; left: 290px; top: 3300px;">
+				<img src="../wp-content/themes/twentyfifteen/photo/bln_photo11.jpg" style="width: 200px; margin-top: -33.4px;">
+			</div><!-- 11 -->
+		</div>
+		<div id="inner-cover">
+				<header>
+					<div id="site-header">
+						<h1>
+							<img src="../wp-content/themes/twentyfifteen/images/idx-sitehead.png" width="789" height="479" />
+						</h1>
+					</div>
+				</header>
+<!-- 				yellow circle -->
+				<div id="news01" class="news">
+					<div class="article">
+						<a href="" style="text-decoration: none">
+							<h4 style="margin-left:50px;">お知らせ</h4>
+								<dl>
+									<dt>2014年6月25日</dt>
+									<dd>島主コラムを更新しました。</dd>
+								</dl>
+							<img src="../wp-content/themes/twentyfifteen/images/tmp_arrow-a.png">
+						</a>
+					</div>
+				</div>
+				<div id="news02" class="news">
+					<div class="article">
+						<a href="" style="text-decoration: none">
+							<h4 style="margin-left:50px;">お知らせ</h4>
+								<dl>
+									<dt>2014年10月8日</dt>
+									<dd>カオハガン・ハウス求人　2014年12月末まで</dd>
+								</dl>
+							<img src="../wp-content/themes/twentyfifteen/images/tmp_arrow-a.png">
+						</a>
+					</div>
+				</div>
+		</div>
+		<footer >
+			<div id="foot-container" style="top: 3612px; background-image:url('../wp-content/themes/twentyfifteen/images/foot_bk.png')">
+				<img src="../wp-content/themes/twentyfifteen/images/foot_bird1.png" width="122" height="112" id="bird1">
+				<img src="../wp-content/themes/twentyfifteen/images/foot_bird2.png" width="118" height="104" id="bird2">
+				<img src="../wp-content/themes/twentyfifteen/images/foot_tree.png" width="162" height="190" id="tree">
+				<img src="../wp-content/themes/twentyfifteen/images/foot_logo.png" alt="Caohagan Island" width="344" height="75" id="logo">
+				<img src="../wp-content/themes/twentyfifteen/images/foot_copyright.png" alt="Copyright(C)2013 Caohagan Island All Rights Reserved." width="280" height="11" id="copyright">
+			</div>
+		</footer>
+	</div>
+	
 </body>
